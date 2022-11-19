983df6a94235fade0b9b4b4d758ba60f78149e50
Carghai74 <carghai74@gmail.com>
2022-11-19 00:31:06 -0500

    tinkering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let result = new()
+        .args(["-c","./global_ssh",])
+        .output().
+        unwrap()
+        .stdout;
+    println!("{}", String::from_utf8(result).unwrap());
+}
+
+fn new() -> Command {
+    Command::new("bash")
+}
+
+pub fn get() -> String {
+    let mut return_data = String::new();
+    io::stdin()
+        .read_line(&mut return_data)
+        .expect("Failed to read input");
+    return_data
 }
