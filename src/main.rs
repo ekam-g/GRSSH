c20d5a45b81c30aebde20d9c03e414231648c74f
carghai <carghai74@gmail.com>
2022-11-23 11:32:35 -0500

    command test

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,14 @@ use std::io;
 use std::process::Command;
 
 fn main() {
-    let result = new()
-        .args(["-c","./global_ssh",])
-        .output().
-        unwrap()
-        .stdout;
-    println!("{}", String::from_utf8(result).unwrap());
+    loop {
+        println!("{}",command(get()));
+    }
 }
 
-fn new() -> Command {
-    Command::new("bash")
-}
+// fn new() -> Command {
+//     Command::new("bash")
+// }
 
 pub fn get() -> String {
     let mut return_data = String::new();
@@ -20,4 +17,35 @@ pub fn get() -> String {
         .read_line(&mut return_data)
         .expect("Failed to read input");
     return_data
+}
+pub fn command(what: String) -> String {
+    let (first, rest) = {
+        let (mut first, mut rest) = ("", vec![]);
+        let first_done = false;
+        for word in what.split_whitespace() {
+            if !first_done {
+                first = word;
+            } else {
+                rest.push(word)
+            }
+        }
+        (first, rest)
+    };
+    let success = Command::new(first).args(rest).output();
+
+    match success {
+        Ok(good) => {
+            String::from_utf8(good.stdout).unwrap()
+        }
+        Err(_) => {
+            let val = Command::new("bash")
+                .args(["-c", "./global_ssh"])
+                .args(what.split_whitespace())
+                .output()
+                .unwrap()
+                .stdout;
+
+            String::from_utf8(val).unwrap()
+        }
+    }
 }
