bf681f1f77a373c4ea06d1b59fafd23206c304f5
carghai <carghai74@gmail.com>
2022-12-05 14:18:34 -0500

    mid progress commit adding kill command

--- src/host.rs
+++ src/host.rs
@@ -1,20 +1,36 @@
 use std::{thread, time};
 
 use crate::command::exc;
-use crate::db::get_command_thread::get_command;
+use crate::db::get_command_thread::{get_command, check_command};
 use crate::db::send;
-use crate::ram_var::HOST_VAR;
+use crate::ram_var::HostData;
 
 
 pub fn host_main() {
     loop {
+        reset();
         let data = get_command();
         let thread_worker = thread::spawn(move || {
             let result = exc(data);
-            
+            let mut pub_data = HostData::get();
+            pub_data.done = true;
+            pub_data.data = result;
         });
-        let result = exc(data);
+        let result: String = loop {
+            let pub_data = HostData::get();
+            if pub_data.done == true {
+                break pub_data.data.clone();
+            }
+            
+        };
         send(&format!("**{}", result)).unwrap();
         thread::sleep(time::Duration::from_secs(1));
     }
+}
+
+fn reset() {
+    let mut data = HostData::get();
+    data.kill_thread = false;
+    data.done = false;
+    data.data = String::new();
 }
