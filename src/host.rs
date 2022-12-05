d71a96a0b723baada60e7e11146c26016488f36d
carghai <carghai74@gmail.com>
2022-12-05 13:53:42 -0500

    adding kill funtion

--- src/host.rs
+++ src/host.rs
@@ -3,13 +3,18 @@ use std::{thread, time};
 use crate::command::exc;
 use crate::db::get_command_thread::get_command;
 use crate::db::send;
+use crate::ram_var::HOST_VAR;
+
 
 pub fn host_main() {
     loop {
         let data = get_command();
+        let thread_worker = thread::spawn(move || {
+            let result = exc(data);
+            
+        });
         let result = exc(data);
         send(&format!("**{}", result)).unwrap();
         thread::sleep(time::Duration::from_secs(1));
     }
 }
-
