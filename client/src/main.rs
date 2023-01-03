2ec811610b7454080b79bd127d7bbfb41f00dbcd
carghai <carghai74@gmail.com>
2023-01-03 15:32:21 -0500

    encriptied a ton

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,6 +8,8 @@ mod client;
 mod db;
 mod input;
 mod ram_var;
+mod tests;
+
 const LOCATION_TO_REDIS_KEY: &str = "redis_key.txt";
 const ENCRYPTION: Encrypt =  Encrypt{
     key : "hello",
---
+++ client/src/tests/mod.rs
@@ -0,0 +1,8 @@
+use crate::db::get;ppp
+
+#[test]
+fn test_data() {
+    let fetch = get();
+    println!("{fetch}");
+
+}
