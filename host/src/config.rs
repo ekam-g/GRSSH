b3bb93fa814305466c82208e5702ea1cb95752b3
Carghai74 <94583299+carghai@users.noreply.github.com>
2023-01-04 22:47:39 -0500

    added comments

--- host/src/config.rs
+++ host/src/config.rs
@@ -15,6 +15,8 @@ pub const ENCRYPTION: crate::db::Encrypt =  crate::db::Encrypt{
 };
 // If you want sentry_logging logging information put your key here
 pub const SENTRY: crate::db::sentry_logging::Sentry = crate::db::sentry_logging::Sentry {
+    //your key goes here
     key : "your_sentry_key",
+    // if you want it make it true otherwise let it remain at false
     is_on : false
 };
