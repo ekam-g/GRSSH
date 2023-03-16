2b716c4ee9770b7e48ba42addf01a9d3b0372874
Carghai <carghai74@gmail.com>
2023-03-16 12:39:48 -0400

    ignore

--- client/src/config.rs
+++
@@ -1,9 +0,0 @@
-// redis://<user>:<password>@<endpoint>:<port>
-pub const REDIS_KEY: &str  = "";
-//encryption settings
-pub const ENCRYPTION: crate::db::Encrypt =  crate::db::Encrypt{
-    //your encryption key
-    key : "hello",
-    //if you want encryption
-    encryption_on : true
-};
